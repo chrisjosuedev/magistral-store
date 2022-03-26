9d810d77f95cc71ebfda0d394dd0f9b081464d43
Cristhian Martínez <chrismartinezlara@outlook.es>
2022-03-25 18:59:37 -0600

    Confirmacion al Salir & Regreso, Navegacion

---
+++ public/js/general.js
@@ -0,0 +1,22 @@
+$(function() {
+  
+  function confirmarSalir() {
+    Swal.fire({
+        title: '¿Desea salir de Magistral Store Online?',
+        icon: 'warning',
+        confirmButtonColor: '#3F84FC',
+        cancelButtonColor: '#FC413F',
+        showCancelButton: true,
+        confirmButtonText: 'Salir',
+        cancelButtonText: 'Cancelar',
+      }).then((result) => {
+        if (result.isConfirmed) {   
+            window.location = '/logout'                          
+        }
+    })
+  }   
+
+  $('#logout').on('click', function() {
+      confirmarSalir()
+  })
+})
